8bcab33d50a96fa0d2402fe671c3a1ff52315b79
xx <935774819@qq.com>
2021-01-07 20:26:13 +0800

    优化

--- xiaomi/js/messagepage.js
+++ xiaomi/js/messagepage.js
@@ -1,54 +1,20 @@
-$(function () {
-    getData();
-    $(".bar_right li:nth-child(1) h3").html(
-      `
-      ${cookie.get("name")}
-      `
-    )
-    $(".signr").html(
-      `
-          ${cookie.get("name")}
-          `
-    );
-    $(".info_right > div:nth-child(4) span").html(
-      `
-          ${cookie.get("price")}元
-          `
-    );
-    $(".price_msg").html(
-      `
-          <div><span> ${cookie.get("name")}</span> <i>${cookie.get(
-        "price"
-      )}元</i></div>
-          <strong>总计：${cookie.get("price")}元</strong>
-          `
-    );
-    
-    function getRandid() {
-      return Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
+    function getCart(){
+       var list = localStorage.getItem('cart')||"[]"
+        return JSON.parse(list)
     }
-    $(".gcart>div:nth-child(1)>button:nth-child(1)").click(async function () {
-      // console.log('1111')
-      let data = await $.ajax({
-        url: "../interface/add.php",
-        type: "get",
-        data: {
-          id: getRandid(),
-          name: cookie.get("name"),
-          price: cookie.get("price"),
-          img: cookie.get("src"),
-        },
-        dataType: "json",
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-          console.log(XMLHttpRequest, textStatus, errorThrown);
-        },
-      });
-      console.log(data);
-      if (data.code === 1) {
-        alert("商品添加成功");
-      } else {
-        alert("商品添加失败");
-      }
-      // console.log(data)
-    });
-  });
+    function setCart(arr){
+        localStorage.setItem('cart',JSON.stringify(arr))
+    }
+    $('#cartin').click(function(){
+        console.log(1);
+        var  newProduct ={
+            product_img:$('#cartin').data('img'),
+            product_id:$('#cartin').data('id'),
+            product_name:$('#cartin').data('name'),
+            product_price:$('#cartin').data('price'),
+            product_num:$('#cartin').data('num')
+        };
+        var productList =getCart();
+        productList.push(newProduct);
+        setCart(productList)
+    })
